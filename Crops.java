63241a299a95da984be20874887436de298acefb
Jasman Sidhu <80021667+jman479@users.noreply.github.com>
2021-04-19 01:26:14 -0500

    Create Crops.java

---
+++ Crops.java
@@ -0,0 +1,6 @@
+// Crops.java
+
+
+// What should be in this file:
+// Variables: crop_name, state, start_date, water_per_week, sunlight
+// Methods: Add_Crop_Array, Remove_Crop_Array, Update_Crop_Array
