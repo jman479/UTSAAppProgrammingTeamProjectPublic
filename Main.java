ec55f8a43534a928ab37bb9f058600bc9a4edb25
Jasman Sidhu <80021667+jman479@users.noreply.github.com>
2021-04-19 01:14:35 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,13 @@
-Hello
-
-//I am editing
+/*
+ * Our term project is centered around making an app to help catalog the plants you would like to grow
+ * and offer tips on how to take care of such plants.
+ * 
+ * Members of this group include:
+ * Michael Ginsberg - sfi208
+ * Aisha Bah - ozg281
+ * David Maestas - xzu322
+ * Hunter Long - mia014
+ * Jasman Sidhu - tqx609
+ * UTSA CS 3443.002 - Application Term Project
+ * Spring 2021
+ */
