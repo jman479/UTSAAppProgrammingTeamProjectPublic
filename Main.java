86b2586645cebaa39febcb2c681b1f5cf870a00b
MichaelGinsberg <michaelnginsberg@gmail.com>
2021-04-26 16:05:31 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,8 +10,27 @@
  * Jasman Sidhu - tqx609
  * UTSA CS 3443.002 - Application Term Project
  * Spring 2021
- */
+*/
+package sample;
 
-// What should be in this file:
-// Variables: date, season
-// Methods: Login, New_Field, Existing_Field, & Main
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        primaryStage.setTitle("Plants for Life");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
