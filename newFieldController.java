e559b2b29fd01a923ad290426ae183852169eef3
MichaelGinsberg <michaelnginsberg@gmail.com>
2021-04-27 12:48:36 -0500

    Update newFieldController.java

--- newFieldController.java
+++ newFieldController.java
@@ -6,8 +6,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
+import java.io.*;
 
 public class newFieldController
 {
@@ -21,12 +24,29 @@ public class newFieldController
     private TextField locationTF;
     @FXML
     private TextField dateTF;
+    @FXML
+    private Label messageLabel;
 
-    public void createFieldButtonAction(ActionEvent event)
+    public newFieldController (String filename)
     {
+        String filenameU = filename;
+    }
+
+    /**
+     * createFieldButtonAction - Takes in the users mouse click and will check if all
+     * text fields are filled in. If they are then the program will call another
+     * function to store all the users information, but if the fields are not filled in
+     * then the program will prompt the user to fill in the missing fields
+     * @param event - mouse click
+     */
+    public void createFieldButtonAction(ActionEvent event) throws IOException {
         if (!fieldNameTF.getText().isBlank() && !locationTF.getText().isBlank() && !dateTF.getText().isBlank())
         {
             inputField();
+        }
+        else
+        {
+            messageLabel.setText("Please fill out all fields, then try again");
         }
     }
 
@@ -41,12 +61,22 @@ public class newFieldController
         stage.close();
     }
 
-    public void inputField()
-    {
+    public void inputField() throws IOException {
+        String filenameU = "";
+
+        FileWriter fieldWriter = new FileWriter(filenameU, true);
+        fieldWriter.append("\n");
+        fieldWriter.append(fieldNameTF.getText());
+        fieldWriter.append(",");
+        fieldWriter.append(locationTF.getText());
+        fieldWriter.append(",");
+        fieldWriter.append(dateTF.getText());
+        fieldWriter.flush();
+        fieldWriter.close();
+
         fieldOverviewPage();
         Stage stage = (Stage) createFieldB.getScene().getWindow();
         stage.close();
-        // input field data into csv and move onto overview page
     }
 
     /**
