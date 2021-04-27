267fafec0daf8821b1e09e0c6d567d74a84082cb
MichaelGinsberg <michaelnginsberg@gmail.com>
2021-04-27 09:52:15 -0500

    Update LoginController.java
    Added links to both overview and new account pages

--- LoginController.java
+++ LoginController.java
@@ -11,6 +11,9 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
@@ -33,6 +36,8 @@ public class LoginController
     @FXML
     private Button loginB;
     @FXML
+    private Button newAccountB;
+    @FXML
     private Label messageText;
     @FXML
     private TextField usernameTF;
@@ -43,7 +48,7 @@ public class LoginController
      * loginButtonAction - Takes in the users mouse click and will check if all
      * text fields are filled in. If they are then the program will call another
      * function to validate the login request, but if both fields are not filled in
-     * then the program will prompt the user to fill in the missing fields
+     * then the program will prompt the user to fill in the missing fields.
      * @param event - mouse click
      */
     public void loginButtonAction(ActionEvent event)
@@ -60,7 +65,7 @@ public class LoginController
 
     /**
      * exitButtonAction will cause the window to close if the user
-     * presses the exit button
+     * presses the exit button.
      * @param event - mouse click
      */
     public void exitButtonAction(ActionEvent event)
@@ -70,11 +75,23 @@ public class LoginController
     }
 
     /**
+     * createAccountButtonAction will direct the user to the account creation screen
+     * so they may register and close out the login page once they are redirected.
+     * @param event - mouse click
+     */
+    public void createAccountButtonAction(ActionEvent event)
+    {
+        createAccountPage();
+        Stage stage = (Stage) loginB.getScene().getWindow();
+        stage.close();
+    }
+
+    /**
      * validateLogin is called by loginButtonAction() to check the users
      * credentials against all known users in the user_accounts csv. The
      * function will do checks on both the username and passphrase and if they
      * are both correct, will send the user to their overview page. If the user enters
-     * a wrong username or passphrase the function will prompt the user to try again
+     * a wrong username or passphrase the function will prompt the user to try again.
      */
     public void validateLogin()
     {
@@ -122,9 +139,11 @@ public class LoginController
             if (usernameTF.getText().equals(users[i]))
             {
                 isUser = true;
-                if (passphrasePF.getText().equals(users[i + 1]))
-                {
-                    messageText.setText("Login Successful");
+                if (passphrasePF.getText().equals(users[i + 1])) {
+                    //messageText.setText("Login Successful");
+                    fieldOverviewPage();
+                    Stage stage = (Stage) loginB.getScene().getWindow();
+                    stage.close();
                 }
                 else
                 {
@@ -135,6 +154,44 @@ public class LoginController
         if (isUser == true)
         {
             messageText.setText("Incorrect Username or Passphrase");
+        }
+    }
+
+    /**
+     * createAccountPage function will create a new stage and link the fxml
+     * to the newAccount.fxml. It will also open the new stage in a new window.
+     */
+    public void createAccountPage()
+    {
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("newAccount.fxml"));
+            Stage register = new Stage();
+            register.setTitle("Plants for Life");
+            register.setScene(new Scene(root, 600, 400));
+            register.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            e.getCause();
+        }
+    }
+
+    /**
+     * fieldOverviewPage function will create a new stage and link the fxml
+     * to the fieldOverview.fxml. It will also open the new stage in a new window.
+     */
+    public void fieldOverviewPage()
+    {
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("fieldOverview.fxml"));
+            Stage overview = new Stage();
+            overview.setTitle("Plants for Life");
+            overview.setScene(new Scene(root, 600, 400));
+            overview.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            e.getCause();
         }
     }
 }
