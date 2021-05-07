86f5c847115ba7a2347c55dcb2cf5146be1aaecd
Jasman Sidhu <80021667+jman479@users.noreply.github.com>
2021-05-06 21:23:57 -0500

    Delete Crops.java

--- Crops.java
+++
@@ -1,6 +0,0 @@
-// Crops.java
-
-
-// What should be in this file:
-// Variables: crop_name, state, start_date, water_per_week, sunlight
-// Methods: Add_Crop_Array, Remove_Crop_Array, Update_Crop_Array
